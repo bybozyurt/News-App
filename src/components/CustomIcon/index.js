212ee6813f81eba6497839794a28fb79d647eeb8
Ahmet Bozyurt <bozyurtahmet@gmail.com>
2021-09-12 16:16:06 +0300

    CustomIcon edited

--- src/components/CustomIcon/index.js
+++ src/components/CustomIcon/index.js
@@ -3,13 +3,13 @@ import {TouchableOpacity, View} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 
-export default function CustomIcon({name,color, style, onPress}) {
+export default function CustomIcon({name,color, style,}) {
 
     return (
         <View style={{...style}}>
-            <TouchableOpacity onPress={onPress}>
-                <Icon name={name} color={color} size={25} />
-            </TouchableOpacity>
+            
+            <Icon name={name} color={color} size={25} />
+            
         </View>
         
     );
